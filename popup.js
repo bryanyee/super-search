de375d13bd5d0cb53ddac180a2079e86c6673124
Bryan Yee <yee.bryan.c@gmail.com>
2016-08-18 15:04:13 -0700

    Add audio files (not functional)

--- popup.js
+++ popup.js
@@ -5,6 +5,16 @@ document.addEventListener('DOMContentLoaded', () => {
   searchBox.addEventListener('keydown', (e) => {
     if (e.key === 'Enter') {
      
+      // let audio = document.createElement('audio');
+      // audio.setAttribute('src', 'D2.mp3');
+      // audio.setAttribute('id', 'audio');
+      // document.body.appendChild(audio);
+      // audio.play();
+
+      let audio = new Audio('D2.mp3');
+      //document.body.appendChild(audio);
+      audio.play();
+
       let searchTerm = searchBox.value.replace(/\s/g, '+');
 
       const query = 'https://www.google.com/search?start=0&num=10&q=' + searchTerm;
--- scrape.js
+++ scrape.js
@@ -1,16 +1,32 @@
+// Challenges
+// - Retrieving google search results - google API didn't work out
+// - Infinite loops when creating tabs
+// - Retrieve links on the webpage by a common class name
+
+// Features
+// 1) Search function
+// 2) Display top 3 search results
+// Started using the chrome extension to make additional searches
+
+
+// allow option to keep or remove the google results page
+
+// remove all body children
+// insert top three links into a single webpage
+// toggle to other links
+// replace bad pages
+
+
+let audio = new Audio('D2.mp3');
+//document.body.appendChild(audio);
+audio.play();
 
 var elements = document.getElementsByClassName('r');
 var linksArray = Array.from(elements);
 
-console.log('linksArray len', linksArray.length);
-
 var actualLinksArray = linksArray.map(function(item) {
   return item.childNodes[0].attributes[0].nodeValue;
 });
-
-console.log('actualLinksArray length', actualLinksArray.length);
-console.log(actualLinksArray);
-
 
 for(var i = 0; i < 3; i++) {
   var link = document.createElement("a");
@@ -23,4 +39,6 @@ for(var i = 0; i < 3; i++) {
   document.getElementById('actualLink' + i).click();
 }
 
-       
+//window.close();
+
+
